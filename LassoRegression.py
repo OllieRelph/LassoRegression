48dfbc590efd2b2fdfe2752e35514af7ae3c156d
OllieRelph <43878641+OllieRelph@users.noreply.github.com>
2020-10-02 15:22:06 +0100

    Add files via upload

---
+++ LassoRegression.py
@@ -0,0 +1,52 @@
+#Regression models with randomly creeated data.
+from sklearn import linear_model, metrics
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+    
+
+def create_y_data(variance):
+    y_data = []
+    for x in range(1000):
+        y_data.append(x + (random.randint(0,variance)))
+    return y_data 
+
+def lasso_regression(y_data, variance):
+    x_data = []
+    for x in range(1000):
+        x_data.append(x)
+    x_data = np.array(x_data).transpose()
+    y_data = np.array(y_data).transpose()
+    y_train = y_data[:-200]
+    y_test =  y_data[-200:]
+    x_train = x_data[:-200]
+    x_test = x_data[-200:]
+    
+    
+    
+    lassoreg = linear_model.Lasso()
+    lassoreg.fit(x_train.reshape(-1,1), y_train)
+    y_prediction = lassoreg.predict(x_test.reshape(-1,1))
+    MSE = metrics.mean_squared_error(y_test,y_prediction)
+    r2score = metrics.r2_score(y_test,y_prediction)
+    
+    
+    
+    plt.scatter(x_test, y_test, color = "black")
+    plt.plot(x_test, y_prediction, color = "red")
+    plt.ylabel("Output")
+    plt.xlabel("Input")
+    plt.title("Lasso Regression, Rnd Y Change = %s" %variance)
+    plt.show()
+    print("Mean Squared Error = %.2f" %MSE)
+    print("R2 Score = %.2f" %r2score)
+
+
+lasso_regression(create_y_data(1), "1")
+
+lasso_regression(create_y_data(10), "10")
+
+lasso_regression(create_y_data(50), "50")
+
+lasso_regression(create_y_data(100), "100")
